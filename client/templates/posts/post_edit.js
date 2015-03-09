3f710f7eb381e881678748e4dd70dc486103d146
frank0000 <frank_oh@hotmail.com>
2015-03-08 18:47:14 -0700

    Only allow changing certain fields of posts.

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,37 @@
+Template.postEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var currentPostId = this._id;
+    
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+    
+    var postWithSameLink = Posts.findOne({url: postProperties.url});
+    if (postWithSameLink && postWithSameLink._id != currentPostId) {
+      alert("Post with this url already exists");
+      return;
+    }
+
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Router.go('postPage', {_id: currentPostId});
+      }
+    });
+  },
+  
+  'click .delete': function(e) {
+    e.preventDefault();
+    
+    if (confirm("Delete this post?")) {
+      var currentPostId = this._id;
+      Posts.remove(currentPostId);
+      Router.go('postsList');
+    }
+  }
+});
